02ea15b97a21b6fc083af21a8b5442757d86ac51
imiancunningham <122496390+imiancunningham@users.noreply.github.com>
2023-01-17 13:53:02 -0800

    Add files via upload

---
+++ Hellow World testing server.java
@@ -0,0 +1,9 @@
+public class Main {
+    public void sayingHello(){
+        System.out.println("Hello World");
+    }
+}
+public static void main(string args[]){
+    Main message = new Main();
+    message.sayingHello();
+}
